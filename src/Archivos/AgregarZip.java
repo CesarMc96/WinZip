6b3ce2754b305c5f9a38b3f93b0e2d02dc503f9c
cesar_000 <cesar_000@192.168.0.11>
2017-06-11 14:27:21 -0500

    Comprime varios archivos

--- src/Archivos/AgregarZip.java
+++ src/Archivos/AgregarZip.java
@@ -1,6 +1,5 @@
 package Archivos;
 
-import java.io.*;
 import java.util.ArrayList;
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
@@ -9,33 +8,21 @@ import net.lingala.zip4j.util.Zip4jConstants;
 
 public class AgregarZip {
 
-    public AgregarZip(String rutaArchivo) {
+    public AgregarZip(ArrayList archivosAgregar) {
 
         try {
-
             ZipFile zipFile = new ZipFile("C:\\Users\\cesar_000\\Documents\\NetBeansProjects\\Estructura de Datos\\Ordinario//Comprimido.zip");
 
-            ArrayList archivosAgregar = new ArrayList();
-            //Archivo a agregar
-            archivosAgregar.add(new File(rutaArchivo));
-
             ZipParameters parametros = new ZipParameters();
-
             parametros.setCompressionMethod(Zip4jConstants.COMP_DEFLATE); // set compression method to deflate compression
-
             parametros.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
-
             parametros.setEncryptFiles(true);
-
             parametros.setEncryptionMethod(Zip4jConstants.ENC_METHOD_AES);
-
             parametros.setAesKeyStrength(Zip4jConstants.AES_STRENGTH_256);
-
             parametros.setPassword("123");
 
             zipFile.addFiles(archivosAgregar, parametros);
         } catch (ZipException e) {
         }
     }
-
 }
--- src/Ventana/Ventana.java
+++ src/Ventana/Ventana.java
@@ -4,14 +4,17 @@ import Archivos.AgregarZip;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Ventana extends JFrame {
 
@@ -23,38 +26,99 @@ public class Ventana extends JFrame {
     private final JLabel lblNombreArchivo;
     private final JPanel pnlAA;
     private final JPanel pnlAB;
-    private final JPanel pnlAbajo; 
-    private String rutaFichero;
-    
+    private final JPanel pnlAbajo;
+    private final JLabel lblPregunta;
+    private final JTextField txtPregunta;
+    private final JButton btnAceptar;
+    private final JPanel pnlAC;
+    private final JPanel pnlAD;
+    private Integer numeroArchivos;
+    ArrayList archivosAgregar = new ArrayList();
+    private final JPanel pnlContraseña;
+    private final JLabel lblContraseña;
+    private final String[] Opciones = {"", "Si", "No"};
+    private final JComboBox ListaOpcion;
+    private final JButton btnOpcion;
+    ArrayList nombreArchivos = new ArrayList();
+    private JLabel lblEscribeContraseña;
+    private JTextField txtEscribeContraseña;
 
     public Ventana() {
-        super.setSize(420, 150);
+        super.setSize(500, 180);
         super.setLayout(new BorderLayout());
         super.setDefaultCloseOperation(EXIT_ON_CLOSE);
         super.setLocationRelativeTo(null);
         super.setTitle("Compresor de Archivos");
         super.getContentPane().setBackground(Color.WHITE);
 
-        //*************************************
-        btnArchivo = new JButton("Seleccionar Archivo.");
-        lblArchivo = new JLabel("Nombre: ");
-        lblNombreArchivo = new JLabel();
-
+        //***************************************
         pnlArriba = new JPanel();
         pnlArriba.setLayout(new BorderLayout());
         pnlAA = new JPanel();
         pnlAB = new JPanel();
         pnlAA.setBackground(Color.WHITE);
         pnlAB.setBackground(Color.WHITE);
-        pnlAB.add(btnArchivo);
-        pnlAA.add(lblArchivo);
-        pnlAA.add(lblNombreArchivo);
+
+        lblPregunta = new JLabel("¿Cuantos archivos deseas comprimir?");
+        txtPregunta = new JTextField(2);
+        btnAceptar = new JButton("Aceptar");
+
+        pnlAB.add(btnAceptar);
+        pnlAA.add(lblPregunta);
+        pnlAA.add(txtPregunta);
+
         pnlArriba.add(pnlAA, BorderLayout.PAGE_START);
         pnlArriba.add(pnlAB, BorderLayout.PAGE_END);
 
-        btnArchivo.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
+        //***************************************
+        pnlAC = new JPanel();
+        pnlAD = new JPanel();
+        btnArchivo = new JButton("Seleccionar Archivo.");
+        lblArchivo = new JLabel("Archivos: ");
+        lblNombreArchivo = new JLabel();
+        pnlAC.setBackground(Color.WHITE);
+        pnlAD.setBackground(Color.WHITE);
+
+        btnComprimir = new JButton("Comprimir");
+        btnDescomprimir = new JButton("Descomprimir");
+
+        pnlAbajo = new JPanel();
+        pnlAbajo.setBackground(Color.WHITE);
+        pnlAbajo.add(btnComprimir);
+        pnlAbajo.add(btnDescomprimir);
+        pnlAbajo.setVisible(false);
+
+        //***************************************
+        pnlContraseña = new JPanel();
+        pnlContraseña.setBackground(Color.WHITE);
+
+        lblContraseña = new JLabel("¿Deseas agregarle contraseña a los archivos?");
+        ListaOpcion = new JComboBox(Opciones);
+        ListaOpcion.setBackground(Color.white);
+        btnOpcion = new JButton("Aceptar");
+
+        pnlContraseña.add(lblContraseña);
+        pnlContraseña.add(ListaOpcion);
+        pnlContraseña.add(btnOpcion);
+
+        //***************************************
+        btnAceptar.addActionListener((ActionEvent ae) -> {
+            pnlAC.add(lblArchivo);
+            pnlAC.add(lblNombreArchivo);
+            pnlAD.add(btnArchivo);
+
+            pnlArriba.add(pnlAC, BorderLayout.PAGE_START);
+            pnlArriba.add(pnlAD, BorderLayout.PAGE_END);
+            pnlAA.setVisible(false);
+            pnlAB.setVisible(false);
+
+            numeroArchivos = Integer.parseInt(txtPregunta.getText());
+        });
+
+        //***************************************
+        btnArchivo.addActionListener((ActionEvent ae) -> {
+
+            for (int i = 0; i < numeroArchivos; i++) {
                 JFileChooser fc = new JFileChooser();
                 int seleccion = fc.showOpenDialog(fc);
 
@@ -63,37 +127,48 @@ public class Ventana extends JFrame {
                     File fichero = fc.getSelectedFile();
 
                     //Escribe el nombre del fichero
-                    lblNombreArchivo.setText(fichero.getName());
-                    
-                    rutaFichero = fichero.getAbsolutePath();
+                    nombreArchivos.add(fichero.getName());
+                    archivosAgregar.add(new File(fichero.getAbsolutePath()));
                 }
             }
+
+//            lblNombreArchivo.setText((String) nombreArchivos.get(0));
+            String nombree = (String) nombreArchivos.get(0);
+            
+            for (int i = 1; i < numeroArchivos; i++) {
+                nombree = nombree + "   ||   " + nombreArchivos.get(i);
+                lblNombreArchivo.setText(nombree);
+            }
+
+            pnlContraseña.setVisible(true);
+            super.add(pnlContraseña, BorderLayout.PAGE_END);
+
         });
 
-        //*************************************
-        btnComprimir = new JButton("Comprimir");
-        
-        btnComprimir.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                new AgregarZip(rutaFichero);       
+        //***************************************
+        btnComprimir.addActionListener((ActionEvent ae) -> {
+            AgregarZip agregarZip = new AgregarZip(archivosAgregar);
+            JOptionPane.showMessageDialog(this, "Archivo Comprimido");
+        });
+
+        //***************************************
+        btnDescomprimir.addActionListener((ActionEvent ae) -> {
+            System.out.println(archivosAgregar);
+        });
+
+        //***************************************
+        btnOpcion.addActionListener((ActionEvent ae) -> {
+            if (ListaOpcion.getSelectedItem() == "Si") {
+                lblEscribeContraseña = new JLabel("Contraseña: ");
+                txtEscribeContraseña = new JTextField(8);
+                
+                
             }
         });
-        
-        btnDescomprimir = new JButton("Descomprimir");
-
-        pnlAbajo = new JPanel();
-        pnlAbajo.setBackground(Color.WHITE);
-        pnlAbajo.add(btnComprimir);
-        pnlAbajo.add(btnDescomprimir);
 
         super.add(pnlArriba, BorderLayout.PAGE_START);
         super.add(pnlAbajo, BorderLayout.PAGE_END);
         super.setVisible(true);
-    }
-
-    public String getRutaFichero() {
-        return rutaFichero;
     }
 
 }
